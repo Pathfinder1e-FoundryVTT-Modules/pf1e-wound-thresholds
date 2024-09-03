3af41cd10cccbd2f358cbea3984719cf6ab0aaab
KitCat <katsulynx@gmail.com>
2024-09-03 16:11:10 +0100

    Simplify condition registry and prep for settings

--- modules/registry/conditions.mjs
+++ modules/registry/conditions.mjs
@@ -1,79 +1,35 @@
 import {MODULE_ID} from "../_moduleId.mjs";
 
 export function registerConditions(registry) {
-    registry.tracks.push("wounded")
+    registry.tracks.push("wounded");
+
+    const modifier = 2;
+    const offsets = {
+        cl: -2
+    };
 
     registry.register(MODULE_ID, "wtGrazed", {
         name: "PF1WT.WoundThresholdsGrazed",
         texture: "modules/pf1e-wound-thresholds/icons/conditions/open-wound.svg",
         mechanics: {
-            changes: [
-                {
-                    formula: -2,
-                    target: "attack",
-                    modifier: "penalty"
-                },
-                {
-                    formula: -2,
-                    target: "allSavingThrows",
-                    modifier: "penalty"
-                },
-                {
-                    formula: -2,
-                    target: "skills",
-                    modifier: "penalty",
-                },
-                {
-                    formula: -2,
-                    target: "allChecks",
-                    modifier: "penalty",
-                },
-                {
-                    formula: -2,
-                    target: "ac",
-                    modifier: "penalty",
-                }
-            ]
+            changes: ["attack", "allSavingThrows", "skills", "allChecks", "ac", "cl"].map(target => ({
+                formula: -1 * modifier * (offsets[target] || 1),
+                target: target,
+                modifier: "penalty"
+            })),
         },
         track: "wounded"
-    })
+    });
 
     registry.register(MODULE_ID, "wtWounded", {
         name: "PF1WT.WoundThresholdsWounded",
         texture: "modules/pf1e-wound-thresholds/icons/conditions/ragged-wound.svg",
         mechanics: {
-            changes: [
-                {
-                    formula: -4,
-                    target: "attack",
-                    modifier: "penalty"
-                },
-                {
-                    formula: -4,
-                    target: "allSavingThrows",
-                    modifier: "penalty"
-                },
-                {
-                    formula: -4,
-                    target: "skills",
-                    modifier: "penalty",
-                },
-                {
-                    formula: -4,
-                    target: "allChecks",
-                    modifier: "penalty",
-                },
-                {
-                    formula: -4,
-                    target: "ac",
-                    modifier: "penalty",
-                },
-                {
-                    formula: -2,
-                    target: "cl",
-                    modifier: "penalty",
-                },
-            ]
+            changes: ["attack", "allSavingThrows", "skills", "allChecks", "ac", "cl"].map(target => ({
+                formula: -2 * modifier * (offsets[target] || 1),
+                target: target,
+                modifier: "penalty"
+            })),
         },
         track: "wounded"
     })
@@ -82,38 +38,11 @@ export function registerConditions(registry) {
         name: "PF1WT.WoundThresholdsCritical",
         texture: "modules/pf1e-wound-thresholds/icons/conditions/bloody-stash.svg",
         mechanics: {
-            changes: [
-                {
-                    formula: -6,
-                    target: "attack",
-                    modifier: "penalty"
-                },
-                {
-                    formula: -6,
-                    target: "allSavingThrows",
-                    modifier: "penalty"
-                },
-                {
-                    formula: -6,
-                    target: "skills",
-                    modifier: "penalty",
-                },
-                {
-                    formula: -6,
-                    target: "allChecks",
-                    modifier: "penalty",
-                },
-                {
-                    formula: -6,
-                    target: "ac",
-                    modifier: "penalty",
-                },
-                {
-                    formula: -4,
-                    target: "cl",
-                    modifier: "penalty",
-                },
-            ]
+            changes: ["attack", "allSavingThrows", "skills", "allChecks", "ac", "cl"].map(target => ({
+                formula: -3 * modifier * (offsets[target] || 1),
+                target: target,
+                modifier: "penalty"
+            })),
         },
         track: "wounded"
     })
